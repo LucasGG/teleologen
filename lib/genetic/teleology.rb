cf6a691cc49de0cdbae390f2bc6a73fa560ada76
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-10-18 19:29:19 -0300

    add some comments

--- lib/genetic/teleology.rb
+++ lib/genetic/teleology.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class Genetic
+  ##
   # Fitness function.
+
   class Teleology
     attr_reader :function
 
     def initialize(&function)
       @function = function
     end
+
+    ##
+    # Apply teleology.
 
     def calculate(behavior)
       @function.call(behavior).to_f
