c29acf7cf149f99597f57e75642b797f55cb7037
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2021-03-16 20:15:38 -0300

    add more complete tests for mutation

--- test/test_mutation.rb
+++ test/test_mutation.rb
@@ -5,16 +5,26 @@ require 'minitest/autorun'
 require 'teleologen'
 
 class TestMutation < Minitest::Test
-  NUMBER_OF_ASSERTIONS = 100
-
   def test_child
-    individuals = Array.new(100) do
+    Teleologen.random_source = Random.new(12_938)
+
+    assert new_mutated_child_genome, '00000000010000111111101100010011'
+    assert new_mutated_child_genome, '00000000010010001100000001011101'
+    assert new_mutated_child_genome, '00000000000010110100111100111111'
+    assert new_mutated_child_genome, '00000000100000000011011010110001'
+    assert new_mutated_child_genome, '00000000001011000111110110011001'
+  end
+
+  private
+
+  def parents
+    @parents ||= Array.new(100) do
       Teleologen::Individual.new(Teleologen.rand(0..10_000_000)) { |first_parameter| first_parameter }
     end
+  end
 
-    NUMBER_OF_ASSERTIONS.times do
-      parent = individuals.sample
-      assert Teleologen::Mutation.new(parent).child.genome.size, parent.genome.size
-    end
+  def new_mutated_child_genome
+    parent = parents.sample(random: Teleologen.random_source)
+    Teleologen::Mutation.new(parent).child.genome
   end
 end
